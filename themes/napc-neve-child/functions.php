48bb253f35ddf2775727c9ec951c19c077389680
Paul Hart <atomicjeep@gmail.com>
2019-12-17 14:50:04 +0000

    tweak render

--- themes/napc-neve-child/functions.php
+++ themes/napc-neve-child/functions.php
@@ -34,7 +34,7 @@ add_shortcode('sg_logo', function () {
 	// $context = Timber::get_context();
     // $post = new TimberPost();
     // $context['post'] = $post;
-    return Timber::render( 'sg-logo.twig' );
+    Timber::render( 'sg-logo.twig' );
 
     // $out = '';
     // return $out;
