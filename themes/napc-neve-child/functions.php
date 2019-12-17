ba40d5796a5adfdcbd5b5828d7073b2538bc3430
Paul Hart <atomicjeep@gmail.com>
2019-12-17 10:25:47 +0000

    edit button

--- themes/napc-neve-child/functions.php
+++ themes/napc-neve-child/functions.php
@@ -12,3 +12,11 @@ if ( ! function_exists( 'napc_neve_child_load_css' ) ):
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'napc_neve_child_load_css', 20 );
+
+
+
+// Shortcode to render edit button where we want it
+add_shortcode('napc_edit_button', function () {
+    $out = edit_post_link('Edit This', '', '', '', 'napc-edit-button');
+    return $out;
+});
