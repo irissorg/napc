3a591ef88e15b99470853be49a9488fcfd207509
Paul <atomicjeep@gmail.com>
2020-05-13 19:45:09 +0100

    updated path

--- themes/napc-neve-child/functions.php
+++ themes/napc-neve-child/functions.php
@@ -14,7 +14,7 @@ if ( ! function_exists( 'napc_neve_child_load' ) ):
 	 */
 	function napc_neve_child_load() {
 		wp_enqueue_style( 'napc-neve-child-style', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'neve-style' ), CHILD_THEME_NAPC_VERSION );
-		wp_enqueue_script('napc-theme-js', get_stylesheet_directory_uri() . 'napc-focus.js', array(), CHILD_THEME_NAPC_VERSION, 'all');
+		wp_enqueue_script('napc-theme-js', get_stylesheet_directory_uri() . '/napc-focus.js', array(), CHILD_THEME_NAPC_VERSION, 'all');
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'napc_neve_child_load', 20 );
